4dafbda3e67a8fe5cf45c026bc21775481f4a724
Danny Hooftman <58477073+dhooftman72@users.noreply.github.com>
2020-08-23 11:21:19 +0200

    Add files via upload

--- GIStatsMain.m
+++ GIStatsMain.m
@@ -2,62 +2,91 @@ function GIStatsMain(type)
 clc
 warning off
 tic
+cd('TheData')
 load('GI_data.mat');
+cd ..
 List = LisFunc;
 %Radiation paper
-List.Traits_Species = [1,2,5,7,9,11,13,15,17,19,23,24];
+List.Traits_Species = [1,5,7,9,11,13,15,17,19,23,24];
 %ES paper
 %List.Traits_Species_ES = [1,2,25,27,38,40,28,29,30,31,32,18,33,34,35];
 %%
-Input.species_option = type; % which species
-Input.Linear_option = 0; % all segment elements
-Input.dis_max = 8; % counter on scenarios
-
-MakeMeanTraits(Input,List); % Creates file
-CalcStatsRadi; % Creates file
+List.species_option = type; % which species; 
+%Type 1 = single core; Type 2= Any core; Type 3 = all species; 
+% Type 4 = not present single target; Type 5: all incoming species
+List.Linear_option = 0; % all segment elements
+List.dis_max = 8; % counter on scenarios
+List.loop_max_Predict = 300; % Prediction Loops
+List.sense_factor = 0.25; % sensitivity factor
+if exist('Radiation_proportions.mat') == 0
+MakeMeanTraits(Input,List); % Creates outputfile: Radiation_proportions.mat
+else
+     testr = testRadi(type);
+     if testr == 1
+         MakeMeanTraits(Input,List); % Creates outputfile: Radiation_proportions.mat
+     end
+end
+CalcStatsRadi; % Creates output file: Output_Anova.mat
 namefile = ['Anova_',char(List.typeList(type)),'.mat'];
 copyfile('Output_Anova.mat',namefile);
-delete('Output_Anova.mat') 
 namefile = ['TraitsFile_',char(List.typeList(type)),'.mat'];
 copyfile('Radiation_proportions.mat',namefile);
-delete('Radiation_proportions.mat')
 
-PredictFits; % this creates Predictions file for ES paper
+%% ES Paper
+if exist('folder_out','dir') == 0
+    mkdir('folder_out')
+end
+PredictFitMain(Input,List); % Creates output file:Predictions.mat
 namefile = ['Predictions_',char(List.typeList(type)),'.mat'];
 copyfile('Predictions.mat',namefile);
+delete('Output_Anova.mat') 
+delete('Radiation_proportions.mat')
 delete('Predictions.mat')
+clc
+display('  ')
+display('  ')
 display('***********    READY   **********')
 end
 
+function testr = testRadi(type)
+load('Radiation_proportions.mat')
+testr = 0;
+if List.species_option ~= type;
+    testr = 1;
+end
+end
 %%
 function List = LisFunc
 %% Numbering traits (presence, cover)
-% "Radiation paper"
-% 1     : Amount of Species, Numeric, single: Presence only.
-% 2     : Species Diversity Shannon,Numeric, single
-% 3-4   : Grime, Categorical: translation needed for Triangle
-% 5-6   : Mean Grime triangle, Numeric, 3-Axis scores
-% 7-8   : Mean max dispeRsal (Tamme et al), Numeric, single
-% 19-20 : Ellenberg, Numeric, multiple independent traits
-% 21-22 : Habitat preference, Categorical, translation needed for Triangle
-% 23-24 : Mean Habitat Preference as Triangle, Numeric, 3-Axis scores
+% 1     : Amount of Species, Numeric, single: Presence only. (Both papers)
+% 2     : Species Diversity Shannon,Numeric, single (ES paper)
+
+% "Radiation Paper" traits
+% 3-4   : Grime, Categorical: translation needed for Triangle (abundance/cover)
+% 5-6   : Mean Grime triangle, Numeric, 3-Axis scores (abundance/cover)
+% 7-8   : Mean max dispeRsal (Tamme et al), Numeric, single (abundance/cover)
+% 19-20 : Ellenberg, Numeric, multiple independent traits (abundance/cover)
+% 21-22 : Habitat preference, Categorical, translation needed for Triangle (abundance/cover)
+% 23-24 : Mean Habitat Preference as Triangle, Numeric, 3-Axis scores (abundance/cover)
 % For Tamme et al (dispeRsal)
-% 9-10  : Mean Terminal Velocity, Numeric, single
-% 11-12 : Mean Seed Dry weight, , Numeric, single
-% 13-14 : Dispersal syndrome for dispeRsal, Categorical
-% 15-16 : Growth form for dispeRsal, Categorical
-% 17-18 : Release heigh, Numeric, single
-% Ecosystem services not above 
-% 25    : Total Nectar Index, incl Cover, Numeric, single Cover only
-% 26-27 : Amount of PollinatorVisits, Numeric, single
-% 28    : Mean Carbon dry weight per leaf, Numeric, single Cover only
-% 29    : Mean Dry weight per leaf, Numeric, single Cover only
-% 30    : Mean Carbon Dry weight Root, Numeric, single Cover only
-% 31    : Mean Dry weight Root, Numeric, single Cover only
-% 32    : Mean Rooting Depth Dry weight Root, Numeric, single Cover only
-% 33    : LandScape diversity Numeric, single
-% 34    : Life Form Shannon Diversity, Numeric, single Cover only
-% 35    : Colour Shannon diversity in # colours, Numeric, single Cover only
+% 9-10  : Mean Terminal Velocity, Numeric (abundance/cover)
+% 11-12 : Mean Seed Dry weight, , Numeric (abundance/cover)
+% 13-14 : Dispersal syndrome for dispeRsal, Categorical (abundance/cover)
+% 15-16 : Growth form for dispeRsal, Categorical (abundance/cover)
+% 17-18 : Release heigh, Numeric(also ES!!) (abundance/cover)
+
+% "Ecosystem services paper traits, not above" 
+% All Numeric & Cover corrected
+% 25    : Total Nectar Index, incl Cover 
+% 27    : Amount of PollinatorVisits
+% 28    : Mean Carbon dry weight per leaf
+% 29    : Mean Dry weight per leaf
+% 30    : Mean Carbon Dry weight Root
+% 31    : Mean Dry weight Root, Numeric
+% 32    : Mean Rooting Depth Dry weight Root
+% 33    : LandScape diversity (no plant information!!)
+% 34    : Life Form Shannon Diversity
+% 35    : Colour Shannon diversity in # colours
 %% LIST to transfer
 List.trait_txts = {'Onetraittxt','Onetraittxt','Grimetxt','Grimetxt','GrimeCentroidtxt',... %5
                     'GrimeCentroidtxt','Onetraittxt','Onetraittxt','Onetraittxt','Onetraittxt',... %10
